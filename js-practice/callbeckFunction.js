aea292066ee247ac8952be3df5646aab318c0edc
MD Nayem <nayemtp@gmail.com>
2024-01-19 22:40:58 +0600

    js practice

---
+++ js-practice/callbeckFunction.js
@@ -0,0 +1,37 @@
+const paymentSuccess = true;
+const marks = 90;
+
+function enroll(callbeck) {
+  console.log("Course enrollment is in progress .");
+  setTimeout(function () {
+    if (paymentSuccess) {
+      callbeck();
+    } else {
+      console.log("payment failed");
+    }
+  }, 2000);
+}
+
+function progress(callbeck) {
+  console.log("Course on progress");  
+  setTimeout(function () {
+    if (marks >= 80) {
+      callbeck();
+    } else {
+      console.log('You could not get enough marks to get the certificate');
+    }
+  }, 3000);
+}
+function getCertificate() {
+  console.log("Progress your certificate");
+  setTimeout(function () {
+    console.log('Congrats! You got the certificate');
+  }, 1000);
+}
+
+// enroll(progress);
+// progress(getCertificate);
+
+enroll(function () {        //anonimus function call system 
+    progress(getCertificate);
+});
