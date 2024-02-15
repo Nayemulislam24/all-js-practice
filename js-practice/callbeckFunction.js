1b737860904ab4cd75493f783ab2f28c3dff4fe1
MD Nayem <nayemtp@gmail.com>
2024-02-15 21:55:37 +0600

    js practice

--- js-practice/callbeckFunction.js
+++ js-practice/callbeckFunction.js
@@ -30,7 +30,10 @@ function getCertificate() {
 }
 
 // enroll(progress);
-// progress(getCertificate);\
+// progress(getCertificate);
+// progress(getCertificate);
+// progress(getCertificate);
+// progress(getCertificate);
 
 enroll(function () {        //anonimus function call system 
     progress(getCertificate);
